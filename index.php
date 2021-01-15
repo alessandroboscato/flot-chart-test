1e5e058928cfccc0592e0cca829ac43052522b0e
Alessandro <alessandro.boscato@gmail.com>
2021-01-15 14:39:52 +0100

    included flot.time.js

--- index.php
+++ index.php
@@ -43,23 +43,24 @@ if (!$result) {
     die('Invalid query: ' . mysql_error());
 }
 
-// collect data by month date
-$num_rows = mysql_num_rows($result);
-var_dump($num_rows);
 
-
-
-if(isset($_GET["startdate"]) && isset($_GET["enddate"]))
-{
-    // print table
-          echo "<table>"; // start a table tag in the HTML
-          $result = mysql_query($query);
-          while ($row = mysql_fetch_assoc($result))
-          {
-          echo "<tr><td>" . $row['order_id'] . "</td><td>" . $row['order_date'] . "</td></tr>";
-          }
-          echo "</table>";
-}
+// collect data
+// $num_rows = mysql_num_rows($result); //total orders
+// var_dump($num_rows);
+//
+//
+// //print table
+// if(isset($_GET["startdate"]) && isset($_GET["enddate"]))
+// {
+//     // print table
+//           echo "<table>"; // start a table tag in the HTML
+//           $result = mysql_query($query);
+//           while ($row = mysql_fetch_assoc($result))
+//           {
+//           echo "<tr><td>" . $row['order_id'] . "</td><td>" . $row['order_date'] . "</td></tr>";
+//           }
+//           echo "</table>";
+// }
 
 
 mysql_close($con);
@@ -88,8 +89,11 @@ mysql_close($con);
 <div id="placeholder" style="width:600px;height:300px"></div>
 
 
+
+
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.3/jquery.flot.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/flot/0.8.1/jquery.flot.time.min.js "></script>
   <script type="text/javascript" src="main.js">
 
   </script>
--- main.js
+++ main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
 
-$.plot($("#placeholder"), [ [[0, 0], [0, 1], [1, 1]], [[0, 1], [1, 10]] ], { yaxis: { max: 10 } });
+  var date = new Date('2021/01/15');
+  var time = date.getTime() / 1000;
+  console.log(time);
+
+
+// $.plot(placeholder, data, options);
+monthNames: ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"];
+$.plot($("#placeholder"), [ [["jan", 0], ["feb", 1]], [["jan", 1], ["feb", 10]] ], {
+    xaxes: [ { mode: "time" } ],
+    yaxes: [ { }, { } ]
+} );
+
+
+
+
+// $.plot($("#placeholder"), [ [[0, 0], [0, 1], [1, 1]], [[0, 1], [1, 10]] ], { yaxis: { max: 10 } });
+
 
 });
